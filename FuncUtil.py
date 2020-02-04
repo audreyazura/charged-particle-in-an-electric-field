90ab39c48f9df3e01773776d834bfdad9a877b13
Audrey Azura <audrey.azura@protonmail.com>
2020-02-04 20:58:56 +0900

    Adding license

--- FuncUtil.py
+++ FuncUtil.py
@@ -1,3 +1,19 @@
+#Particle Movement: calculate the movement of a charged particle in an electric field given as input
+#    Copyright (C) 2020  Alban Lafuente-Sampietro
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #Extract columns of data from a file
 #Warning: only work for numerical data
 #Warning: ignores no-utf8 characters. If you want them to raise an error, remove the errors="ignore" parameter in the file opening
--- ParticleMovements.py
+++ ParticleMovements.py
@@ -1,3 +1,19 @@
+#Particle Movement: calculate the movement of a charged particle in an electric field given as input
+#    Copyright (C) 2020  Alban Lafuente-Sampietro
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from FuncUtil import *
 from math import sqrt
 from numpy.random import normal
